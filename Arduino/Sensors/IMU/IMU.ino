3ea14e37f412e3d2dd9451ef68c911fe1518228e
AntonioCervantes <acervantes.eng@gmail.com>
2022-04-08 21:50:21 -0700

    Add IMU

---
+++ Arduino/Sensors/IMU/IMU.ino
@@ -0,0 +1,19 @@
+#include "MPU9250.h"
+
+MPU9250 mpu; // You can also use MPU9255 as is
+
+void setup() {
+    Serial.begin(115200);
+    Wire.begin();
+    delay(2000);
+
+    mpu.setup(0x68);  // change to your own address
+}
+
+void loop() {
+    if (mpu.update()) {
+        Serial.print(mpu.getYaw()); Serial.print(", ");
+        Serial.print(mpu.getPitch()); Serial.print(", ");
+        Serial.println(mpu.getRoll());
+    }
+}
